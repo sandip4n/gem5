424998b2c77f61b240ce6594cfe770d0782041b1
Sandipan Das <sandipan@linux.vnet.ibm.com>
2018-06-07 18:39:20 +0530

    arch-power: Add fixed-point doubleword shift instructions
    This adds the following shift instructions:
      * Shift Left Doubleword (sld[.])
      * Shift Right Doubleword (srd[.])
      * Shift Right Algebraic Doubleword (srad[.])
      * Shift Right Algebraic Doubleword Immediate (sradi[.])
      * Extend-Sign Word and Shift Left Immediate (extswsli[.])
    
    Change-Id: Icd1f3efda715c5b8a7c7bc648ba29a8749e74695
    Signed-off-by: Sandipan Das <sandipan@linux.vnet.ibm.com>

--- src/arch/power/insts/integer.cc
+++ src/arch/power/insts/integer.cc
@@ -630,6 +630,70 @@ IntShiftOp::generateDisassembly(Addr pc, const SymbolTable *symtab) const
 
 
 string
+IntConcatShiftOp::generateDisassembly(Addr pc, const SymbolTable *symtab) const
+{
+    stringstream ss;
+    bool printSecondSrc = true;
+    bool printShift = false;
+
+    // Generate the correct mnemonic
+    string myMnemonic(mnemonic);
+
+    // Special cases
+    if (!myMnemonic.compare("sradi") ||
+        !myMnemonic.compare("extswsli")) {
+        printSecondSrc = false;
+        printShift = true;
+    }
+
+    // Additional characters depending on isa bits being set
+    if (rcSet) myMnemonic = myMnemonic + ".";
+    ccprintf(ss, "%-10s ", myMnemonic);
+
+
+    // Print the first destination only
+    if (_numDestRegs > 0) {
+        printReg(ss, _destRegIdx[0]);
+    }
+
+    // Print the first source register
+    if (_numSrcRegs > 0) {
+        if (_numDestRegs > 0) {
+            ss << ", ";
+        }
+        printReg(ss, _srcRegIdx[0]);
+
+        // Print the second source register
+        if (printSecondSrc) {
+
+            // If the instruction updates the CR, the destination register
+            // Ra is read and thus, it becomes the second source register
+            // due to its higher precedence over Rb. In this case, it must
+            // be skipped.
+            if (rcSet) {
+                if (_numSrcRegs > 2) {
+                    ss << ", ";
+                    printReg(ss, _srcRegIdx[2]);
+                }
+            } else {
+                if (_numSrcRegs > 1) {
+                    ss << ", ";
+                    printReg(ss, _srcRegIdx[1]);
+                }
+            }
+        }
+    }
+
+    // Print the shift value
+    if (printShift) {
+        ss << ", " << shift;
+    }
+
+    return ss.str();
+}
+
+
+string
 IntRotateOp::generateDisassembly(Addr pc, const SymbolTable *symtab) const
 {
     stringstream ss;
