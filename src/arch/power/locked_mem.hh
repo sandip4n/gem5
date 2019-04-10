e5d027df6c753fd2c891235d5b25bb89b155eb65
Kajoljain379 <kajoljain797@gmail.com>
2019-04-10 05:46:01 +0000

    arch-power: Added support for Atomic Instructions
    Add support for Load and Reserve and Store Conditional Instructions:
    
    * Load Byte And Reserve Indexed.
    * Store Byte Conditional Indexed.
    * Load Halfword And Reserve Indexed.
    * Store Halfword Conditional Indexed.
    * Load Word And Reserve Indexed.
    * Store Word Conditional Indexed.
    * Load Doubleword And Reserve Indexed.
    * Store Doubleword Conditional Indexed.
    
    Change-Id: I1dac94928e7a1bb6f458a4ecea0fca3247b26d37
    Signed-off-by: Kajoljain379 <kajoljain797@gmail.com>

--- src/arch/power/locked_mem.hh
+++ src/arch/power/locked_mem.hh
@@ -41,6 +41,146 @@
  * ISA-specific helper functions for locked memory accesses.
  */
 
-#include "arch/generic/locked_mem.hh"
+#include <cstdlib>
+
+#include "arch/power/miscregs.hh"
+#include "arch/power/registers.hh"
+#include "cpu/thread_context.hh"
+#include "debug/LLSC.hh"
+#include "mem/packet.hh"
+#include "mem/request.hh"
+
+namespace PowerISA
+{
+
+template <class XC>
+inline void
+handleLockedSnoop(XC *xc, PacketPtr pkt, Addr cacheBlockMask)
+{
+    // If we see a snoop come into the CPU and we currently have an LLSC
+    // operation pending we need to clear the lock flag if it is to the same
+    // addr.
+        ThreadContext *tc = xc->getTC();
+        if (!tc->readIntReg(INTREG_RSV))
+        return;
+
+    Addr locked_addr = tc->readIntReg(INTREG_RSV_ADDR);
+    Addr snoop_addr = pkt->getAddr();
+
+    if (locked_addr == snoop_addr)
+        tc->setIntReg(INTREG_RSV, 0);
+}
+
+template <class XC>
+inline void
+handleLockedRead(XC *xc, Request *req)
+{
+    ThreadContext *tc = xc->getTC();
+    tc->setIntReg(INTREG_RSV, 1);
+    tc->setIntReg(INTREG_RSV_LEN, req->getSize());
+    tc->setIntReg(INTREG_RSV_ADDR, req->getPaddr());
+    DPRINTF(LLSC,"%s: Placing addr %#x in monitor\n", xc->getCpuPtr()->name(),
+                 req->getPaddr());
+}
+
+template <class XC>
+inline void
+handleLockedSnoopHit(XC *xc)
+{
+}
+
+
+template <class XC>
+inline bool
+handleLockedWrite(XC *xc, Request *req, Addr cacheBlockMask)
+{
+    DPRINTF(LLSC,"%s: handling locked write for  address %#x in monitor\n",
+            xc->getCpuPtr()->name(), req->getPaddr());
+
+    ThreadContext *tc = xc->getTC();
+    int lock_flag = tc->readIntReg(INTREG_RSV);
+    Addr lock_addr = tc->readIntReg(INTREG_RSV_ADDR);
+    unsigned size = tc->readIntReg(INTREG_RSV_LEN);
+    bool store_performed = false;
+    bool undefined_case;
+
+    if (lock_flag) {
+        if (req->getSize() == size && req->getPaddr() == lock_addr)
+            {
+          undefined_case = false;
+          store_performed = true;
+            }
+            else {
+          //Taking smallest real page size supported as 64k
+              int z = 64*1024;
+              if (req->getPaddr()/z == lock_addr/z)
+                    undefined_case = true;
+              else {
+                undefined_case = false;
+                store_performed = false;
+              }
+            }
+    }
+    else {
+      undefined_case = false;
+      store_performed = false;
+    }
+    Xer xer = tc->readIntReg(INTREG_XER);
+    Cr cr = tc->readIntReg(INTREG_CR);
+    tc->setIntReg(INTREG_RSV, 0);
+
+    if (undefined_case) {
+            bool randombool = rand() % 1;
+            if (randombool){
+              xc->setStCondFailures(0);
+            }
+            bool secondrandombool = rand() % 1;
+            cr.cr0 = ((secondrandombool ? 0x2 : 0x0) | xer.so);
+            tc->setIntReg(INTREG_CR, cr);
+    return randombool;
+    }
+
+    if (store_performed) {
+        xc->setStCondFailures(0);
+    }
+      else {
+      // Lock flag not set or addr mismatch in CPU;
+      // the rest of this code is not architectural;
+      // it's just a debugging aid to help detect
+      // livelock by warning on long sequences of failed
+      // store conditionals
+      int stCondFailures = xc->readStCondFailures();
+      stCondFailures++;
+      xc->setStCondFailures(stCondFailures);
+      if (stCondFailures % 100000 == 0) {
+      warn("%i: context %d: %d consecutive "
+             "store conditional failures\n",
+             curTick(), xc->contextId(), stCondFailures);
+      }
+
+      if (!lock_flag){
+        DPRINTF(LLSC, "[cid:%i]: Lock Flag Set, "
+                "Store Conditional Failed.\n",
+                 req->contextId());
+      }
+      else if (req->getPaddr() != lock_addr) {
+             DPRINTF(LLSC, "[cid:%i]: Load-Link Address Mismatch, "
+                              "Store Conditional Failed.\n",
+                        req->contextId());
+      }
+      }
+      cr.cr0 = ((store_performed ? 0x2 : 0x0) | xer.so);
+      tc->setIntReg(INTREG_CR, cr);
+    // store conditional failed already, so don't issue it to mem
+    return store_performed;
+}
+
+template <class XC>
+inline void
+globalClearExclusive(XC *xc)
+{
+}
+
+} // namespace PowerISA
 
 #endif // __ARCH_POWER_LOCKED_MEM_HH__
